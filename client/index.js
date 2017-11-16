56406853239d8ab3c76ddc306666b1c8eb00dc88
Jakub <jakub.kraska95@gmail.com>
2017-11-17 00:10:35 +0100

    small fix

--- client/index.js
+++ client/index.js
@@ -6,13 +6,19 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import reducers from './reducers';
 import App from './components/App';
-import './scss/main.scss'
+import Header from './components/Header';
+import $ from "jquery";
+
+import './scss/main.scss';
+import 'bootstrap';
+
 const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
 
 const renderApp = () => (
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router>
       <main>
+        <Header />
         <Switch>
           <Route exact path="/" component={App} />
         </Switch>
